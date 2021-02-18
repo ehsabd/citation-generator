43eaabbda9b88893de0fc7f9f6f47ea0a77daa10
ehsabd <ehsabd@outlook.com>
2021-02-18 13:13:56 +0330

    Add author button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JournalArticleComponent } from './journal-article/journal-article.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'journal-article', component:JournalArticleComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/journal-article/journal-article.component.ts
+++ src/app/journal-article/journal-article.component.ts
@@ -8,10 +8,14 @@ import {Author} from '../author';
 export class JournalArticleComponent implements OnInit {
 
   authors : Author[] = [new Author()]
-
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addAuthor(): void{
+    this.authors.push(new Author());
   }
 
 }
