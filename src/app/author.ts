7c80f3edd09dbd3f2d44ffbafc989b9dc2e48868
ehsabd <ehsabd@outlook.com>
2021-02-22 22:27:10 +0330

    toString in Author

--- src/app/author.ts
+++ src/app/author.ts
@@ -1,6 +1,9 @@
 export class Author {
     initials:string = '';
     lastName:string = '';
+    public toString = () : string => {
+        return `${this.lastName}, ${this.initials}`;
+    }
 }
   
   
