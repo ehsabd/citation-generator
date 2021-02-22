db576b633cbbafb6391bb771ccf3289c5f915f80
ehsabd <ehsabd@outlook.com>
2021-02-22 17:14:44 +0330

    add JournalArticle type

---
+++ src/app/journal-article.ts
@@ -0,0 +1,17 @@
+import { Author } from "./author";
+
+export class JournalArticle {
+    title = '';
+    authors: Author[] = [];
+    journalTitle = '';
+    fromPage = '';
+    toPage = '';
+    volume = '';
+    issue = '';
+    year = 2020;
+    doi = '';
+    url = '';
+    isInPress = false;
+}
+  
+  
