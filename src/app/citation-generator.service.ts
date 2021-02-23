0a8a3f23f9ccfa6f974c0976ce6d9b4696a3df85
ehsabd <ehsabd@outlook.com>
2021-02-23 12:29:50 +0330

    fixed one author mode

--- src/app/citation-generator.service.ts
+++ src/app/citation-generator.service.ts
@@ -10,7 +10,10 @@ export class CitationGeneratorService {
 
   getAuthorsPart(source: JournalArticle){
     const authors = source.authors;
-    if (authors.length<20){
+    if (authors.length==1){
+      return this.getAuthorString(authors[0]);
+    }
+    else if (authors.length<20){
         return `${authors.slice(0, authors.length-1).map(a=>this.getAuthorString(a)).join(', ')}, & ${this.getAuthorString(authors[authors.length-1])}`;
     }else{
         return '';//TODO
