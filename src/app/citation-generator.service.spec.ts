8f5b7665b05d75d3207873f787a8afa3af71df1f
ehsabd <ehsabd@outlook.com>
2021-02-23 12:24:14 +0330

    update authorspart test, adding one author and three authors modes

--- src/app/citation-generator.service.spec.ts
+++ src/app/citation-generator.service.spec.ts
@@ -8,16 +8,23 @@ describe('CitationGeneratorService',()=>{
     beforeEach(() => { service = new CitationGeneratorService(); });
 
     it ('should return authors part correctly', ()=>{
-        let authors:Author[] = [];
-        authors.push(
+        let threeauthors:Author[] = [];
+        threeauthors.push(
             {initials:'J.', lastName:'Doe'},
-            {initials:'M.',  lastName:'Major'});
+            {initials:'M.',  lastName:'Major'},
+            {initials:'E. Z.', lastName:'Carpenter'});
+
+        let twoauthors = threeauthors.slice(0,2);
+        let oneauthor = threeauthors.slice(0,1);
         let journalArticle = new JournalArticle();
-        journalArticle.authors = authors;
+        journalArticle.authors = threeauthors;
+        expect(service.getAuthorsPart(journalArticle)).toEqual('Doe, J., Major, M., & Carpenter, E. Z.');
+        journalArticle.authors = twoauthors;
         expect(service.getAuthorsPart(journalArticle)).toEqual('Doe, J., & Major, M.');
+        journalArticle.authors = oneauthor;
+        expect(service.getAuthorsPart(journalArticle)).toEqual('Doe, J.');
 
-
-
+            
     })
 
 })
