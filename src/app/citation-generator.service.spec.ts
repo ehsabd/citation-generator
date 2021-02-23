7269f3ec5a07a0492a5f214b7fd3bbbe30dd883e
ehsabd <ehsabd@outlook.com>
2021-02-23 12:43:53 +0330

    article citation test spec added

--- src/app/citation-generator.service.spec.ts
+++ src/app/citation-generator.service.spec.ts
@@ -27,4 +27,23 @@ describe('CitationGeneratorService',()=>{
             
     })
 
+    it ('should return citation correctly for article1', ()=>{
+        let a = new JournalArticle();
+        a.authors.push({initials:'T.', lastName:'Nguyen'})
+        a.authors.push({initials:'J. J.', lastName:'Carnevale'})
+        a.authors.push({initials:'A. A.', lastName:'Scholer'})
+        a.authors.push({initials:'D. B.', lastName:'Miele'})
+        a.authors.push({initials:'K.', lastName:'Fujita'})
+
+        a.title = 'Metamotivational knowledge of the role of high-level and low-level construal in goal-relevant task performance';
+        a.journalTitle = 'Journal of Personality and Social Psychology';
+        a.volume = '117';
+        a.issue = '5';
+        a.fromPage = '879';
+        a.toPage = '899';
+        a.doi = 'http://dx.doi.org/10.1037/pspa000016'
+
+        expect(service.getCitation(a)).toEqual('Nguyen, T., Carnevale, J. J., Scholer, A. A., Miele, D. B., & Fujita, K. (2019). Metamotivational knowledge of the role of high-level and low-level construal in goal-relevant task performance. Journal of Personality and Social Psychology, 117(5), 879-899. http://dx.doi.org/10.1037/pspa0000166')
+    })
+
 })
