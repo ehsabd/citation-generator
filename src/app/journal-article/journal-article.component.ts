a9f183d912363f06f1a3c5d9aee899657644ad46
ehsabd <ehsabd@outlook.com>
2021-02-26 14:39:13 +0330

    Add template bindings, update citation using `ngAfterViewChecked`

--- src/app/journal-article/journal-article.component.ts
+++ src/app/journal-article/journal-article.component.ts
@@ -1,20 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import {Author} from '../author';
+import { JournalArticle } from '../journal-article';
+import {CitationGeneratorService} from '../citation-generator.service';
 @Component({
   selector: 'app-journal-article',
   templateUrl: './journal-article.component.html',
-  styleUrls: ['./journal-article.component.scss']
+  styleUrls: ['./journal-article.component.scss'],
+  providers: [CitationGeneratorService]
 })
-export class JournalArticleComponent implements OnInit {
 
-  authors : Author[] = [new Author()]
-  
-  constructor() { }
+export class JournalArticleComponent extends JournalArticle implements OnInit, AfterViewChecked {
 
-  ngOnInit(): void {
+  citation = ''
+
+  constructor(private citationGeneratorService:CitationGeneratorService) { 
+    super()
+    this.addAuthor()
   }
 
-  addAuthor(): void{
+  ngOnInit(): void {
+    this.citationGeneratorService = new CitationGeneratorService();
+  }
+
+  ngAfterViewChecked(): void{
+    setTimeout(()=>{
+      this.citation = this.citationGeneratorService.getCitation(this);
+    },0)
+    
+  }
+
+    addAuthor(): void{
     this.authors.push(new Author());
   }
 
